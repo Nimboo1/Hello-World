1231a50c3ba1558021c424b08828e05681abf593
Nimboo1 <43417650+Nimboo1@users.noreply.github.com>
2018-12-19 21:04:53 +0300

    Update list.h

--- list.h
+++ list.h
@@ -1,2 +1,24 @@
+typedef unsigned char BYTE;
 
-1
+struct bmp
+	{
+		char type[2];
+		int size,reserved,head_end,header_size,weight,height;
+		int end[7];
+	};
+
+struct bmp image[10];
+FILE * file_input[10];
+int i,j,z,number,captchasize;
+int weight_captcha;
+char captcha[30];
+
+struct bmp fileRead(FILE* inputeFile);
+
+char* nameCreate(char fileSymbol /*captcha[i]*/);
+void fileWrite (struct bmp bmpImage, int captchaSize, BYTE ***rastrCaptcha, BYTE padArray[][20], int *padding, int captchaWeight);
+int mainWork(int captchasize,struct bmp image[10], int *padding, FILE * file_input[10], BYTE pad_array[][20], BYTE ***rastr_captcha);
+
+void mainWork2(int weight, int w, struct bmp image, BYTE ***rastr1, BYTE ***rastr2, BYTE ***rastr);
+void fileWrite1(struct bmp bmpImage, int weight, BYTE ***rastr, BYTE pad[20], int padding, FILE * fout);
+
